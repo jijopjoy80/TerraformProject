6cb4d2669020b026826c0b4fb2bc05576e776915
askkrishnaprasad <120196234+askkrishnaprasad@users.noreply.github.com>
2023-06-07 20:23:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -88,12 +88,13 @@ resource "aws_instance" "web" {
               sudo apt-get update
               sudo apt-get install -y nginx
               sudo cp /home/jenkins/terraform-aws/index.html /var/www/html/index.html
-              sudo chown www-data:www/data /var/www/html/index.html
+              sudo chown www-data:www-data /var/www/html/index.html
               sudo chown -R www-data:www-data /var/www/html
               sudo find /var/www/html -type d -exec chmod 755 {} \;
               sudo find /var/www/html -type f -exec chmod 644 {} \;
               sudo mv /var/www/html/index.nginx-debian.html /var/www/html/index.nginx-debian.html.bak
               sudo mv /var/www/html/index.html /var/www/html/index.nginx-debian.html
+              sudo chown www-data:www-data /var/www/html/index.nginx-debian.html
               sudo systemctl restart nginx
               EOF
   tags = {
